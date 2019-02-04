aa2169e289d9cef1239d4f273e273b985313d8c0
Pekka Pekkonen <pekka.pekkonen@live.fi>
2019-02-04 12:06:32 +0200

    Add another input and submit button

--- src/App.js
+++ src/App.js
@@ -16,11 +16,15 @@ class App extends React.Component {
           <div className={this.state.entryType==='expense' ? 'link' : null}>Tulot</div>
           <div className={this.state.entryType==='income' ? 'link' : null}>Menot</div>
         </nav>
-        
+
         <h1>{this.state.entryType==='expense' ? 'Menot' : 'Tulot'}</h1>
         <form className="form--add-entry">
           <label>Päivämäärä</label>
           <input type="date"></input>
+          <label>Summa</label>
+          <input type="text"></input>
+          <br></br>
+          <input type="submit" value="+ Lisää"></input>
         </form>
       </div>
     )
