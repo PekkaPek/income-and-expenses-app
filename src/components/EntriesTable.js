47e1632845d89983cac56f97742077002dabeb5b
Pekka Pekkonen <pekka.pekkonen@live.fi>
2019-02-20 14:37:54 +0200

    Show summary details

--- src/components/EntriesTable.js
+++ src/components/EntriesTable.js
@@ -2,13 +2,24 @@ import React from 'react'
 
 const EntriesTable = ( {entries, showEntryType, populateModifyEntryModal, deleteEntry} ) => {
   const entriesToShow = entries.filter(entry => entry.type===showEntryType)
+  const addAmount = (sum, entry) => {
+    return sum + Number(entry.amount)
+  }
   return(
-    <table>
-    <tbody>
-      <tr><th>Päivämäärä</th><th>Summa</th></tr>
+    <div>
+      <table>
+      <tbody>
+        <tr><th>Päivämäärä</th><th>Summa</th></tr>
         {entriesToShow.map(entry => <tr key={entry.id}><td>{new Date(entry.date).toLocaleDateString()}</td><td>{entry.amount.toLocaleString()}</td><td className="link" onClick={deleteEntry(entry)}>Poista</td><td className="link" onClick={populateModifyEntryModal(entry)}>Muokkaa</td></tr>)}
       </tbody>
       </table>
+      <div>
+        {showEntryType === 'expense' ? 'Menoja' : 'Tuloja'} yhteensä {entriesToShow.reduce(addAmount, 0).toLocaleString()} €
+      </div>
+      <div>
+        {entriesToShow.length} tapahtumaa
+      </div>
+    </div>
   )
 }
 
