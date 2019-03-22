a8a576f037ff9f8899e4d2edcd6e5ee04ddecc36
Pekka Pekkonen <pekka.pekkonen@live.fi>
2019-03-22 12:42:43 +0200

    Add styling to add entry form

--- src/components/AddEntryForm.js
+++ src/components/AddEntryForm.js
@@ -3,12 +3,19 @@ import React from 'react'
 const AddEntryForm = ({newEntryDate, newEntryAmount, updateDate, updateAmount, addEntry}) => {
   return (
     <form className="form--add-entry" onSubmit={addEntry}>
-      <label>Päivämäärä</label>
-      <input type="date" value={newEntryDate} onChange={updateDate}></input>
-      <label>Summa</label>
-      <input type="text" value={newEntryAmount} onChange={updateAmount}></input>
-      <br></br>
-      <input type="submit" value="+ Lisää"></input>
+      <div className="form--short-entities-container">
+        <div className="form--short-entity">
+          <label>Päivämäärä</label>
+          <input type="date" value={newEntryDate} onChange={updateDate}></input>
+        </div>
+        <div className="form--short-entity">
+          <label>Summa</label>
+          <input type="text" value={newEntryAmount} onChange={updateAmount}></input>
+        </div>
+      </div>
+      <div className="form--submit-container">
+        <input className="form--submit" type="submit" value="+ Lisää"></input>
+      </div>
     </form>
   )
 }
