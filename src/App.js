23ae7a0f8429597bbf2e0cc4d47ad8a7a736d8f1
Pekka Pekkonen <pekka.pekkonen@live.fi>
2019-03-22 14:26:07 +0200

    Add "add entry" advice section

--- src/App.js
+++ src/App.js
@@ -157,7 +157,7 @@ class App extends React.Component {
         <Navigation showEntryType={this.state.showEntryType} toggleShowEntryType={this.toggleShowEntryType} />
         <div className="content-container">
           <h1>{this.state.showEntryType==='expense' ? 'Menot' : 'Tulot'}</h1>
-          <AddEntryForm newEntryDate={this.state.newEntryDate} newEntryAmount={this.state.newEntryAmount} updateDate={this.updateDate} updateAmount={this.updateAmount} addEntry={this.addEntry} showModifyModal={this.showModifyModal} />
+          <AddEntryForm showEntryType={this.state.showEntryType} toggleShowEntryType={this.toggleShowEntryType} newEntryDate={this.state.newEntryDate} newEntryAmount={this.state.newEntryAmount} updateDate={this.updateDate} updateAmount={this.updateAmount} addEntry={this.addEntry} showModifyModal={this.showModifyModal} />
           <MonthSelector viewPeriod={this.state.viewPeriod} getPreviousMonth={this.getPreviousMonth} getNextMonth={this.getNextMonth}/>
           <IncomeExpenseSummary entries={this.state.entries}/>
           <EntriesTable entries={this.state.entries} showEntryType={this.state.showEntryType} populateModifyEntryModal={this.populateModifyEntryModal} deleteEntry={this.deleteEntry}/>
--- src/components/AddEntryForm.js
+++ src/components/AddEntryForm.js
@@ -1,22 +1,33 @@
 import React from 'react'
 
-const AddEntryForm = ({newEntryDate, newEntryAmount, updateDate, updateAmount, addEntry}) => {
+const AddEntryForm = ({showEntryType, toggleShowEntryType, newEntryDate, newEntryAmount, updateDate, updateAmount, addEntry}) => {
   return (
-    <form className="form--add-entry" onSubmit={addEntry}>
-      <div className="form--short-entities-container">
-        <div className="form--short-entity">
-          <label>Päivämäärä</label>
-          <input type="date" value={newEntryDate} onChange={updateDate}></input>
+    <div className="form--form-container">
+      <div className="form--advice-container">
+        <div className="form--advice-title">
+        Lisää {showEntryType==='expense' ? 'meno' : 'tulo'}
         </div>
-        <div className="form--short-entity">
-          <label>Summa</label>
-          <input type="text" value={newEntryAmount} onChange={updateAmount}></input>
+        <div className="form--advice-text">
+          <p>Vapaaehtoiset kentät on merkattu.</p>
+          <p>{showEntryType==='expense' ? 'Tulon' : 'Menon'} voi lisätä <span className="link" onClick={toggleShowEntryType}>{showEntryType==='expense' ? 'tulo' : 'meno'}näkymästä</span>.</p>
         </div>
       </div>
-      <div className="form--submit-container">
-        <input className="form--submit" type="submit" value="+ Lisää"></input>
-      </div>
-    </form>
+      <form className="form--add-entry" onSubmit={addEntry}>
+        <div className="form--short-entities-container">
+          <div className="form--short-entity">
+            <label>Päivämäärä</label>
+            <input type="date" value={newEntryDate} onChange={updateDate}></input>
+          </div>
+          <div className="form--short-entity">
+            <label>Summa</label>
+            <input type="text" value={newEntryAmount} onChange={updateAmount}></input>
+          </div>
+        </div>
+        <div className="form--submit-container">
+          <input className="form--submit" type="submit" value="+ Lisää"></input>
+        </div>
+      </form>
+    </div>
   )
 }
 
