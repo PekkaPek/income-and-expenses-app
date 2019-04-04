ba3a7858b8afb4105ac36b1ebfd1b51ccd11b787
Pekka Pekkonen <pekka.pekkonen@live.fi>
2019-04-04 19:33:27 +0300

    Add styling to modify entry modal

--- src/components/ModifyEntryModal.js
+++ src/components/ModifyEntryModal.js
@@ -2,10 +2,27 @@ import React from 'react'
 
 const ModifyEntryModal = ({showModifyEntryModal, entryToBeModified, updateEntry, updateEntryToBeModifiedDate, updateEntryToBeModifiedAmount}) => {
   return(
-    <div className={showModifyEntryModal ? 'show' : 'hide'}>
-      <input value={entryToBeModified.date} onChange={updateEntryToBeModifiedDate}/>
-      <input value={entryToBeModified.amount} onChange={updateEntryToBeModifiedAmount}/>
-      <input type="submit" value="Muokkaa" onClick={updateEntry(entryToBeModified)}/>
+    <div className={showModifyEntryModal ? 'modify-entry-modal--container show' : 'hide'}>
+      <div className='modify-entry-modal--background'>
+      </div>
+      <div className='modify-entry-modal--inner-container'>
+        <div className='advice-title'>
+          Muokkaa {entryToBeModified.type === 'income' ? 'tuloa' : 'menoa'}
+        </div>
+        <div className="modify-entry-modal--short-fields-container">
+          <div className="modify-entry-modal--short-field">
+            <label>Päivämäärä</label>
+            <input value={entryToBeModified.date} onChange={updateEntryToBeModifiedDate}/>
+          </div>
+          <div className="modify-entry-modal--short-field">
+            <label>Summa</label>
+            <input value={entryToBeModified.amount} onChange={updateEntryToBeModifiedAmount}/>
+          </div>
+        </div>
+        <div className="modify-entry-modal--submit-container">
+          <input className="submit-button link" type="submit" value="Muokkaa" onClick={updateEntry(entryToBeModified)}/>
+        </div>
+      </div>
     </div>
   )
 }
